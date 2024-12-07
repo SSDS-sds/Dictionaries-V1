53e50472106e91bd92ccff9b841f933429ccc81e
SSDS-sds <warwicklee1@gmail.com>
2024-12-07 18:59:27 +0000

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,15 @@
+#Creating a dictionary
+animals = {"Hamster":"Milktea","Dog":"Mely","Bird":"Sparrow"}
+print(animals)
+
+#Acessing values from a dictionary
+my_bird = animals["Bird"]
+print(f"My pet bird's name is {my_bird}")
+
+#Creating a list of keys
+keys_list = list(animals.keys())
+print(keys_list)
+
+#Creating a list of values
+values_list = list(animals.values())
+print(values_list)
